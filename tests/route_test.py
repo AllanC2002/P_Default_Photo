5817e84bcdb3045b4494e7dcdf4f6c1c7a2fb443
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-07-04 23:41:52 -0500

    add: workflow to prod

--- tests/route_test.py
+++ tests/route_test.py
@@ -1,9 +1,8 @@
 import requests
 
 BASE_URL = "http://107.21.24.33:8080"
-user_id = 12
+user_id = 3
 
-# Enviar user_id en JSON (body) porque el endpoint lo espera así
 response = requests.post(f"{BASE_URL}/default-photo", json={"Id_User": user_id})
 
 print(response.status_code)
