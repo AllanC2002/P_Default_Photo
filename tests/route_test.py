e9635b46db88cb9ed6d810fc93f9197d71da98b6
Allan Correa <134990755+AllanC2002@users.noreply.github.com>
2025-07-06 10:12:50 -0500

    test: prod

--- tests/route_test.py
+++ tests/route_test.py
@@ -1,6 +1,7 @@
 import requests
 
-BASE_URL = "http://107.21.24.33:8080"
+#BASE_URL = "http://107.21.24.33:8080" qa
+BASE_URL = "http://44.210.210.33:8080"
 user_id = 3
 
 response = requests.post(f"{BASE_URL}/default-photo", json={"Id_User": user_id})
